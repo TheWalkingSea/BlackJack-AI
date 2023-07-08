42ffaf1670512d20ffdbb1eb7f31e544ce163ff8
TheWalkingSea <austinmccallister0@gmail.com>
2023-07-08 19:49:16 -0400

    Finished BJ logic
    Finished most of the logic for BJ, just need to organize a game table and also make the AI

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 import string
-
+from typing import Any
 
 
 class Hand:
@@ -60,7 +60,7 @@ class Hand:
             aces -= 1
         return cnt
 
-    def IsBusted(self) -> bool:
+    def isBusted(self) -> bool:
         """Checks if the hand is a bust
         
         Returns:
@@ -86,7 +86,7 @@ class Hand:
         """
         return " ".join(list(map(lambda x: f"{random.choice(self.suits)}{x}", self.cards)))
 
-    def hasBJ(self) -> bool:
+    def checkBJ(self) -> bool:
         """Returns a boolean representing whether or not the hand has BJ
         
         Returns:
@@ -98,6 +98,62 @@ class Hand:
         """
         return sum(self.cards) == 21
     
+class Balance:
+    """Represents a round of blackjack as a dealer"""
+    def __init__(self, initAmt: float, betAmt: float, ratio: tuple=(3, 2)):
+        self.amt = initAmt
+        self.betAmt = betAmt
+        self.ratio = ratio
+        self.flags = {"dd": False, "insure": False}
+
+    def __iadd__(self, other: int):
+        """Adds other and amt"""
+        return self.amt + other
+
+    def __isub__(self, other: int):
+        """Subtracts amt from other"""
+        return self.amt - other
+
+    def bet(self, betAmt: float=None) -> bool:
+        """Subtracts amt from betAmt
+        
+        Parameters:
+        (betAmt): The bet amount, most of the time this will be self.betAmt but may adjust for insurance
+
+        Returns:
+        (bool): Boolean that will be False when there are insufficient funds
+        
+        """
+        if (not betAmt): betAmt = self.betAmt
+        if (self.amt < betAmt): return False
+        self.amt -= betAmt
+        return True
+
+    def payout(self, ratio: tuple=None, bet: float=None) -> float:
+        """Calculates the payout of the bet according to the ratio and changes the balance accordingly
+        
+        Parameters:
+        ratio(tuple): Tuple representing the ratio. Ex: 3:2 means you will win $3 for every $2 you wager. Defaults to self.ratio
+        bet(float): The bet amt in case you doubled down or split, defaults to self.betAmt
+        
+        Returns:
+        (float): The payout
+        """
+        if (not ratio): ratio = self.ratio
+        if (not bet): bet = self.betAmt
+        payout = bet + bet/ratio[1]*ratio[0] # Give money back and then calculate the payout aswell
+        self.amt += payout
+
+        if (self.flags['dd']): # If double down is True, then double the payout
+            self.amt += payout
+            self.flags['dd'] = False # Set the flag to False
+
+        if (self.flags['insure'] and ratio[1] == 0): # If insurance flag is set and the ratio signals pushing
+            self.payout((2, 1), self.betAmt/2) # Insurance is half of a NEW bet and pays 2:1
+            self.flags['insure'] = False
+
+        return payout
+
 class Dealer(Hand):
     def __init__(self):
         super(Hand, self).__init__() # Card 2 will be hidden from the player
@@ -112,13 +168,38 @@ class Dealer(Hand):
         # While 17 is greater than the count just keep hitting, we will deal with whether it busts or not later
         while (self.getCount() < 17): self.hit()
     
+    def checkInsurance(self) -> bool:
+        """Checks whether or not you can insurance is available or when the upCard is an Ace"""
+        return self.getUpCard() == 1
 
-class Player(Hand):
+
+class Player(Hand, Balance):
+    """Represents a round of blackjack as a player"""
     dealer: Dealer = None
 
-    def __init__(self):
+    def __init__(self, balance: Balance):
         super(Hand, self).__init__()
+        self.bal = balance
     
+    def predeterminedWin(self) -> int|None:
+        """Checks for a predetermined win through blackjack represented as an int
+        
+        Returns:
+        (int|None): 1 if the player has BJ, -0 if both the dealer and player have BJ,
+            -1 if the dealer has BJ, and None if neither have BJ
+            
+        """
+        if (self.dealer.checkBJ() and self.checkBJ()): # Push
+            self.bal.payout((1, 0))
+            return 0 
+        if (self.dealer.checkBJ()):
+            return -1
+        if (self.checkBJ()):
+            self.bal.payout()
+            return 1
+        return None
+
+
     def stand(self) -> int:
         """Player stands and this function will tell you if you win or lose
         
@@ -126,12 +207,70 @@ class Player(Hand):
         (int): An integer representing whether you won (1), pushed (0), or loss (-1)
         
         """
-        if (self.isBusted()): return -1 # Checks if the player busts first
+        if (self.isBusted()): 
+            return -1 # Checks if the player busts first
 
         self.dealer.playHand()
-        if (self.dealer.IsBusted()): return 1 # Checks if the dealer busted
+        if (self.dealer.isBusted()): 
+            self.bal.payout()
+            return 1 # Checks if the dealer busted
 
         playerCnt = self.getCount()
         dealerCnt = self.dealer.getCount()
-        if (playerCnt == dealerCnt): return 0 # Push
-        return 1 if playerCnt > dealerCnt else -1 # Finally just check the counts
+        if (playerCnt == dealerCnt): # Push
+            self.bal.payout((1, 0))
+            return 0 
+        
+        # Finally just check the counts
+        if playerCnt > dealerCnt:
+            self.bal.payout()
+            return 1
+        return -1
+
+    def checkSplit():
+        raise NotImplementedError("Splitting is not supported yet")
+    
+    def split():
+        raise NotImplementedError("Splitting is not supported yet")
+
+    def __iadd__(self, other: Any): 
+        raise NotImplementedError("In-place addition has not been implemented for the Player class")
+
+    def __isub__(self, other: Any): 
+        raise NotImplementedError("In-place subtraction has not been implemented for the Player class")
+
+    def doubleDown(self) -> None:
+        """Double bet and in return hit ONE card and stand. Also sets the flag for DD"""
+        self.bal.bet() # Double the bet
+        self.bal.flags['dd'] = True
+        self.hit()
+        self.stand()
+    
+    def checkEvenMoney(self) -> bool:
+        """Checks if the player is eligible to even money; 
+            when you have BJ and the dealer has an Ace as their upcard
+            
+        Note: Even money pays 1 to 1 with a guarenteed win rate but comes at an opportunity cost
+        
+        """
+        return self.checkBJ() and self.dealer.checkInsurance()
+
+    def insure(self) -> None:
+        """Insures your bet by setting insurance flag
+        
+        Note: Insurance works by placing up to half your bet and
+            if the dealer has blackjack the player is paid 2:1.
+            Insurance and the bet are separate
+
+        """
+        self.bal.bet(self.bal.betAmt/2) # You have to bet half the original bet
+        self.bal.flags['insure'] = True
+    
+    def evenMoney(self) -> None:
+        """Changes the balance accordingly to even money, pays out 1:1
+        
+        Pre-req:
+        checkEvenMoney is True
+        
+        """
+        self.bal.payout((1, 1))
